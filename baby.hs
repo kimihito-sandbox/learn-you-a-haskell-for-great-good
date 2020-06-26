f6b2f7c9407ddce3e4728c009d78e67ba62b2a68
kimihito <tatsurotamashiro@gmail.com>
2020-06-27 06:20:32 +0900

    Add circumference (p25)

--- baby.hs
+++ baby.hs
@@ -14,3 +14,6 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
