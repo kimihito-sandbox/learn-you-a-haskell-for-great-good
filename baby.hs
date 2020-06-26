99bc1c45b824a8e413d12eadd5ffbb43ac2c2d96
kimihito <tatsurotamashiro@gmail.com>
2020-06-27 06:19:09 +0900

    p25

--- baby.hs
+++ baby.hs
@@ -11,3 +11,6 @@ doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
 
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
