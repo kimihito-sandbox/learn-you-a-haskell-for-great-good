ef80c2c5885ba54ab598330859bab8c7e5078f4d
kimihito <tatsurotamashiro@gmail.com>
2020-06-27 06:13:06 +0900

    ch1

---
+++ baby.hs
@@ -0,0 +1,13 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x  + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                         then x
+                         else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
