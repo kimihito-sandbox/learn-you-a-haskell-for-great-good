6e2757c4169c9254ae9122ba0ca4deaf22c4e416
kimihito <tatsurotamashiro@gmail.com>
2020-06-30 09:30:16 +0900

    List pattern match (p38)

--- baby.hs
+++ baby.hs
@@ -25,3 +25,9 @@ circumference' r = 2 * pi * r
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy!"
 head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "this list is long. The first two elements are: " ++ show x ++ " and " ++ show y
