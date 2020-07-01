8d44a15e93b5875a402abec481a70da84edf7760
kimihito <tatsurotamashiro@gmail.com>
2020-07-01 09:29:59 +0900

    pattern match case of (p49)

--- baby.hs
+++ baby.hs
@@ -81,3 +81,9 @@ cylinder r h =
     let sideArea = 2 * pi * r * h
         topArea = pi * r ^ 2
     in sideArea + 2 * topArea
+
+describeList :: [a] -> String
+describeList ls = "The list is "
+                  ++ case ls of [] -> "emtpy."
+                                [x] -> "a signleton list."
+                                xs -> "a longer list."
