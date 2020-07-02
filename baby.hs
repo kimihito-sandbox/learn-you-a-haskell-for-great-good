b5a2ddd41be054fb5b74a5a33df2e193c8b2a0fa
kimihito <tatsurotamashiro@gmail.com>
2020-07-02 09:36:03 +0900

    flip' (p68)

--- baby.hs
+++ baby.hs
@@ -142,3 +142,6 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
