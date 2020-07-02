4a62c2f66c450b97d202680b2dff157fbf008cb6
kimihito <tatsurotamashiro@gmail.com>
2020-07-02 09:30:27 +0900

    zipWith' (p67)

--- baby.hs
+++ baby.hs
@@ -137,3 +137,8 @@ multTwoWithNine = multThree 9
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
