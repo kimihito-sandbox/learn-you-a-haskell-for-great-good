8027e0bc30080ebd7227cb71195849e9fcb5de39
kimihito <tatsurotamashiro@gmail.com>
2020-07-03 05:11:00 +0900

    wordNums (p93)

--- ch6.hs
+++ ch6.hs
@@ -2,3 +2,6 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+wordNums :: String -> [(String, Int)]
+wordNums = map (\xs -> (head xs, length xs)) . group . sort . words
