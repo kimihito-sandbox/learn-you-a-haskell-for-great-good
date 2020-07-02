4a677523daa838aa6b3b0c00e3a6d0e7878e3b7a
kimihito <tatsurotamashiro@gmail.com>
2020-07-03 05:57:41 +0900

    findKey using foldr (p101)

--- ch6.hs
+++ ch6.hs
@@ -32,3 +32,8 @@ phoneBook =
     , ("lucille", "205-2928")
     , ("wendy", "939-8282")
     , ("penny", "853-2492")]
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key xs = foldr
+                    (\(k, v) acc -> if key == k then Just v else acc)
+                    Nothing xs
