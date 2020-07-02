d4834894dd5b1f4bf5b0e83a35cd7400fe6ee2ee
kimihito <tatsurotamashiro@gmail.com>
2020-07-03 06:06:12 +0900

    string2digits (p104)

--- ch6.hs
+++ ch6.hs
@@ -40,3 +40,6 @@ findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey key xs = foldr
                     (\(k, v) acc -> if key == k then Just v else acc)
                     Nothing xs
+
+string2digits :: String -> [Int]
+string2digits = map digitToInt . filter isDigit
