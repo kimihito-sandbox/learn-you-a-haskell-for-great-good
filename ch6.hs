8df4c0b95de57d7f0a7b09e11d9a88c0ba40b281
kimihito <tatsurotamashiro@gmail.com>
2020-07-03 05:17:04 +0900

    isIn (p95)

--- ch6.hs
+++ ch6.hs
@@ -5,3 +5,6 @@ numUniques = length . nub
 
 wordNums :: String -> [(String, Int)]
 wordNums = map (\xs -> (head xs, length xs)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
