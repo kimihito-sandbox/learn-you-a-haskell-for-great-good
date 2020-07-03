f70656a827ca6816eb9af57b9b0500fc199ac726
kimihito <tatsurotamashiro@gmail.com>
2020-07-04 06:33:27 +0900

    infixr / infixl (p138)

--- ch7.hs
+++ ch7.hs
@@ -46,4 +46,9 @@ lockers = Map.fromList
     ,(103, (Free, "IQSA9"))
     ]
 
-data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
