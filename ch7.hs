159e1a95863c6a9a143a8453d014ffdb4e44883c
kimihito <tatsurotamashiro@gmail.com>
2020-07-04 05:47:38 +0900

    Vector (p124)

--- ch7.hs
+++ ch7.hs
@@ -12,3 +12,13 @@ data Car  = Car { company :: String
 tellCar :: Car -> String
 tellCar (Car {company = c, model = m, year = y}) = 
     "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+data Vector a = Vector a a a deriving (Show)
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) =  Vector (i+l) (j+m) (k+n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
