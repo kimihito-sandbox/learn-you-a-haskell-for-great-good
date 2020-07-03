af33a0ab6292f5c214e50600fec070b44086571b
kimihito <tatsurotamashiro@gmail.com>
2020-07-03 12:01:42 +0900

    Type parameters (not good) (p123)

--- ch7.hs
+++ ch7.hs
@@ -5,10 +5,10 @@ data Person = Person { firstName :: String
                        , phoneNumber :: String
                        , flavor :: String } deriving(Show)
 
-data Car  = Car { company :: String
-                 , model :: String
-                 , year :: Int                
+data Car a b c = Car { company :: a
+                 , model :: b
+                 , year ::  c               
                  } deriving (Show)
-tellCar :: Car -> String
+tellCar :: (Show a) => Car String String a -> String
 tellCar (Car {company = c, model = m, year = y}) = 
     "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
