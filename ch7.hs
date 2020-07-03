c1e652a72fd34b8f6a1c58fe730f5c0647bd3f7f
kimihito <tatsurotamashiro@gmail.com>
2020-07-04 06:05:06 +0900

    derving(Show, Read) (p128)

--- ch7.hs
+++ ch7.hs
@@ -1,7 +1,7 @@
 data Person = Person { firstName :: String
                        , lastName :: String 
                        , age :: Int
-                       } deriving(Eq)
+                       } deriving(Eq, Show, Read)
 mikeD = Person { firstName = "Michael", lastName = "Diamond", age = 43 }
 adRock = Person { firstName = "Adam", lastName = "Horovitz", age = 41 }
 mca = Person { firstName = "Adam", lastName = "Yauch", age = 44}
