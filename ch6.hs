0df7b3b1deae06c9180461195a728b48fd84fcae
kimihito <tatsurotamashiro@gmail.com>
2020-07-03 10:44:30 +0900

    Using Map.fromListWith (p105)

--- ch6.hs
+++ ch6.hs
@@ -26,15 +26,17 @@ firstTo40 = find (\x -> digitSum x == 40) [1..]
 firstTo :: Int -> Maybe Int
 firstTo n = find (\x -> digitSum x == n) [1..]
 
-phoneBook :: Map.Map String String
 phoneBook = 
-    Map.fromList $ 
     [("betty", "555-2938")
+    , ("betty", "342-2492")
     , ("bonnie", "452-2928")
     , ("patsy", "493-2928")
+    , ("patsy", "943-2929")
+    , ("patsy", "827-9162")
     , ("lucille", "205-2928")
     , ("wendy", "939-8282")
-    , ("penny", "853-2492")]
+    , ("penny", "853-2492")
+    , ("penny", "555-2111")]
 
 findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey key xs = foldr
@@ -43,3 +45,7 @@ findKey key xs = foldr
 
 string2digits :: String -> [Int]
 string2digits = map digitToInt . filter isDigit
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith add xs
+    where add number1 number2 = number1 ++ ", " ++ number2
