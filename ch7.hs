8b71d9105f48b271833f779f7d4d580c29385845
kimihito <tatsurotamashiro@gmail.com>
2020-07-07 08:55:23 +0900

    YesNo instances (p150)

--- ch7.hs
+++ ch7.hs
@@ -71,3 +71,25 @@ treeElem x (Node a left right)
     | x == a = True
     | x < a = treeElem x left
     | x > a = treeElem x right
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False 
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
