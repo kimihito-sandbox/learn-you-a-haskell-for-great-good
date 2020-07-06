363b4b5df95f5ca970316cb01909a71f1728e952
kimihito <tatsurotamashiro@gmail.com>
2020-07-07 06:00:30 +0900

    treeElem (p141)

--- ch7.hs
+++ ch7.hs
@@ -53,14 +53,21 @@ infixr 5 ^++
 Empty ^++ ys = ys
 (x :-: xs) ^++ ys = x :-: (xs ^++ ys)
 
-data Tree a = EmtpyTree | Node a (Tree a) (Tree a) deriving(Show)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving(Show)
 
 singleton :: a -> Tree a
-singleton x = Node x EmtpyTree EmtpyTree
+singleton x = Node x EmptyTree EmptyTree
 
 treeInsert :: (Ord a) => a -> Tree a -> Tree a
-treeInsert x EmtpyTree = singleton x
+treeInsert x EmptyTree = singleton x
 treeInsert x (Node a left right)
     | x == a = Node x left right
     | x < a = Node a (treeInsert x left) right
     | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
